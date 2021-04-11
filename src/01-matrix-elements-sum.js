f47ae65b936769364ad50f5c0feb57d1b38522be
alexandra.harko <sanyuta93@gmail.com>
2021-04-11 23:41:46 +0300

    feat: task "matrix elements sum"

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,17 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let summ = 0;
+  for (let i = 0; i <= matrix.length - 1; i++) {
+    for (let j = 0; j <= matrix[i].length - 1; j++) {
+      if (i === 0 || matrix[i - 1][j] !== 0) {
+        summ += matrix[i][j];
+      }
+    }
+  }
+
+  return summ;
 }
 
 module.exports = getMatrixElementsSum;
--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,15 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  let arr_s1 = s1.split('');
+  let arr_s2 = s2.split('');
+  for (let i=0; i <= arr_s1.length; i++) {
+    if (arr_s2.includes(arr_s1[i])) {
+      arr.push(arr_s1[i]);
+    }
+  }
+  return arr;
 }
 
 module.exports = getCommonCharacterCount;
